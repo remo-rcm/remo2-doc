7c302f51b9af785b7aa9268e5812572804842be8
Lars Buntemeyer <larsbuntemeyer@gmail.com>
2020-07-13 12:11:11 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -60,7 +60,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'REMO Documentation and User Guide'
 copyright = u'2019, Lars Buntemeyer'
-author = [u'Lars Buntemeyer', u'Joni-Pekka Pietikäinen']
+author = u'Lars Buntemeyer, Joni-Pekka Pietikäinen'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
